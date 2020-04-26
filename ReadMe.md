cb53af74188713527ca7f83856da96880df8038d
Robert Fuchs <robertfuchs97@gmail.com>
2020-04-26 08:37:53 +0200

    Initial commit

---
+++ ReadMe.md
@@ -0,0 +1,33 @@
+# 3D Human Pose Estimation
+
+## Setup
+
+- Create a virtual environment and activate it
+
+```
+python -m venv venv
+source venv/bin/activate
+```
+
+- Make sure to install the required python packages 
+
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+Simply execute `main.py` with python:
+
+```
+python main.py
+```
+
+## Troubleshooting
+
+> UserWarning: Matplotlib is currently using agg, which is a non-GUI backend, so cannot show the figure.
+
+If you encounter the error above, you need to install some additional dependencies on your machine. Please see:
+
+- [StackOverflow Thread](https://stackoverflow.com/questions/56656777/userwarning-matplotlib-is-currently-using-agg-which-is-a-non-gui-backend-so)
+- [Arch Linux Forum Thread](https://bbs.archlinux.org/viewtopic.php?pid=1885317#p1885317)
---
+++ main.py
@@ -0,0 +1,42 @@
+# pip install opencv-python matplotlib
+
+
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+import threading
+
+def show_webcam(mirror=False):
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret_val, img = cam.read()
+        if mirror: 
+            img = cv2.flip(img, 1)
+
+        cv2.imshow('Camera Data', img)
+        # Funktioniert noch nicht
+        cv2.circle(img, center=(50, 50), radius=50, color=(255, 0, 255), thickness=5)
+        # cv2.line(img=img, pt1=(10, 10), pt2=(300, 10), color=(255, 0, 0), thickness=50)
+        
+        if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+    cv2.destroyAllWindows()
+
+
+
+mpl.rcParams['legend.fontsize'] = 10
+
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
+z = [2,3]
+x = [2,2]
+y = [1,4]
+ax.plot(x, y, z, label='A Leg (?)')
+ax.legend()
+
+webcamThread = threading.Thread(target=show_webcam);
+webcamThread.start();
+plt.show()
