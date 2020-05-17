5ca96b71e84ffd1d532dcc9a92c4214f596e546b
Marc Schuler <scma1241@hs-karlsruhe.de>
2020-05-17 13:54:07 +0200

    Added model optimizer

--- dataloader.py
+++ dataloader.py
@@ -1,16 +1,12 @@
-import importlib
-import re
-
-import PIL
 import cv2
 import numpy as np
 import torch
-import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 from modules.inference_engine_pytorch import InferenceEnginePyTorch
 from modules.parse_poses import parse_poses
 
 net = None
+
 
 ## Load a model with the given nam
 ## This method is called from the ui (1) on startup and (2) when a user selects a different model
@@ -23,7 +19,12 @@ def loadModel(model):
         print("Warning: Running on CPU")
     else:
         print("CUDA is available")
+    from torch2trt import TRTModule
 
+    if model.endswith("_opt.pth"):
+        print("Loading optimized RTR model")
+        net = TRTModule()
+        net.load_state_dict(torch.load(model))
     net = InferenceEnginePyTorch(model, "GPU" if cuda_active else "CPU")
 
 
@@ -32,17 +33,17 @@ def prepareImage(webcam_image, height, fx):
     if fx < 0:  # Focal length is unknown
         fx = np.float32(0.8 * webcam_image.shape[1])
     image = cv2.cvtColor(webcam_image, cv2.COLOR_BGR2RGB)  # Set TO RGB
-    image = cv2.resize(image, dsize=None, fx=input_scale, fy=input_scale)      # Resize
-    return image,input_scale, fx
+    image = cv2.resize(image, dsize=None, fx=input_scale, fy=input_scale)  # Resize
+    return image, input_scale, fx
+
 
 ## Analyse the webcam image
-def calcPoses(image,input_scale, fx):
+def calcPoses(image, input_scale, fx):
     stride = 8
     inference_result = net.infer(image)
     poses_3d, poses_2d = parse_poses(inference_result, input_scale, stride, fx, True)
 
     return poses_3d, poses_2d
-
 
 
 class WebcamDataset(torch.utils.data.Dataset):
@@ -52,4 +53,4 @@ class WebcamDataset(torch.utils.data.Dataset):
         self.images_class: int = images_class
 
     def __getitem__(self, index):
-        self.transform( self.pil_image, self.images_class) # Transform
+        self.transform(self.pil_image, self.images_class)  # Transform
---
+++ optimizeModel.py
@@ -0,0 +1,22 @@
+# Optimizes a model
+# Relies on the platform => Must be compiled for every device
+# The first command lin parameter must be the path to the old model
+import sys
+
+import torch
+import torch2trt
+
+from models.with_mobilenet import PoseEstimationWithMobileNet
+
+MODEL_NAME = sys.argv[1]
+NEW_MODEL_NAME = MODEL_NAME.replace(".pth", "_opt.pth")
+WIDTH = 224
+HEIGHT = 224
+
+net = PoseEstimationWithMobileNet()
+net.load_state_dict(torch.load(sys.argv[1]))
+
+data = torch.zeros((1, 3, HEIGHT, WIDTH)).cuda()
+
+model_trt = torch2trt.torch2trt(net, [data], fp16_mode=True, max_workspace_size=1 << 25)
+torch.save(model_trt.state_dict(), NEW_MODEL_NAME)
