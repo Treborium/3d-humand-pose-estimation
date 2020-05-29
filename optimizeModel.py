d62ec45d4202bebbf7a681cbe57c6bc586694205
Robert Fuchs <robertfuchs97@gmail.com>
2020-05-29 15:01:39 +0200

    Fix optimize model script

--- optimizeModel.py
+++ optimizeModel.py
@@ -4,9 +4,11 @@
 import sys
 
 import torch
-import torch2trt
+# import torch2trt
 
 from models.with_mobilenet import PoseEstimationWithMobileNet
+from torch2trt.torch2trt import *
+
 
 MODEL_NAME = sys.argv[1]
 NEW_MODEL_NAME = MODEL_NAME.replace(".pth", "_opt.pth")
@@ -14,9 +16,10 @@ WIDTH = 224
 HEIGHT = 224
 
 net = PoseEstimationWithMobileNet()
-net.load_state_dict(torch.load(sys.argv[1]))
+net.load_state_dict(torch.load(sys.argv[1]), strict=False)
+net.eval().cuda()
 
-data = torch.zeros((1, 3, HEIGHT, WIDTH)).cuda()
+data = torch.ones((1, 3, HEIGHT, WIDTH)).cuda()
 
-model_trt = torch2trt.torch2trt(net, [data], fp16_mode=True, max_workspace_size=1 << 25)
+model_trt = torch2trt(net, [data], fp16_mode=True, max_workspace_size=1 << 25)
 torch.save(model_trt.state_dict(), NEW_MODEL_NAME)
